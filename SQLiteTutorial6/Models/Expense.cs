296ec3b10d6a48ef802267ed06f043b18e8dcb5a
Tashwill Advice <tashwilladvice02@gmail.com>
2025-10-28 14:12:46 +0200

    Expense model class created.

--- SQLiteTutorial6/Models/Expense.cs
+++ SQLiteTutorial6/Models/Expense.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,16 @@ namespace SQLiteTutorial6.Models
 {
     public class Expense
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Description { get; set; }
+        public double Amount { get; set; }
+        [MaxLength(50)]
+        public string Category { get; set; }
+        public DateTime Date { get; set; }
+
 
     }
 }
