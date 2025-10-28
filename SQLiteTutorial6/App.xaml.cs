95531ffbdf95520abeab1e446cd8aa47ccf0c808
Tashwill Advice <tashwilladvice02@gmail.com>
2025-10-28 14:40:48 +0200

    database made glovally , path added and initialised in app.xaml.cs

--- SQLiteTutorial6/App.xaml.cs
+++ SQLiteTutorial6/App.xaml.cs
@@ -1,15 +1,23 @@
-﻿namespace SQLiteTutorial6
+﻿using SQLiteTutorial6.Services;
+
+
+namespace SQLiteTutorial6
 {
     public partial class App : Application
     {
+        public static DatabaseService Database { get; private set; }
         public App()
         {
             InitializeComponent();
+            string dbPath = 
+                Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "expenses.db3");
+
+
+            MainPage = new AppShell();
+
         }
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
-        }
+        
     }
 }
