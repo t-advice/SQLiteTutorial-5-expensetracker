994308e5607c5368a3ab5aaefae8d0936591edad
Tashwill Advice <tashwilladvice02@gmail.com>
2025-10-28 12:06:28 +0200

    Nuget Packages installed, two classes created namely Expense.cs (Models) and DatabaseService(Services)  and made they methods public

---
+++ SQLiteTutorial6/Services/DatabaseService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLiteTutorial6.Models;
+
+namespace SQLiteTutorial6.Services
+{
+    public class DatabaseService
+    {
+
+    }
+}
